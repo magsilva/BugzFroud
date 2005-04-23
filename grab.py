da9710bb7efcacc3de2a6293685302caafc57b6e
magsilva <magsilva@a19939ac-6df5-0310-aec8-b42a0800a5a1>
2005-04-23 17:13:53 +0000

    Enabled connection redirection.

--- grab.py
+++ grab.py
@@ -1,38 +1,65 @@
 #!/usr/bin/python
 
-from urllib2 import *
+from httplib import *
 import re
 import os
 import os.path
+import csv
 
-base_url = "http://bugzilla.mozilla.org"
+base_url = "bugzilla.mozilla.org"
+# Can be HTTPConnection also
+connection = HTTPSConnection( base_url )
+connection.connect()
+
 type_re = re.compile( "^text/plain", re.IGNORECASE )
 patch_re = re.compile( "(\.diff\"?$|\.patch\"?$)", re.IGNORECASE )
-destdir = "mozilla"
+destdir = "bugzilla"
 
 if not os.path.exists( destdir ):
 	os.makedirs( destdir )
 
+reader = csv.reader( file( "bugzilla-2005-04-22.csv" ) )
+bugs = []
+for row in reader:
+    bugs.append( int( row[0] ) )
 
-# for i in range(177269,177270):
-for i in range(1,291100):
+for i in bugs:
 	print "\n%d..." %i,
-	url = base_url + "/attachment.cgi?id=" + str(i)
-	attachment = urlopen( url )
-	info = attachment.info()
-	type = info[ "Content-Type" ]
+	url = "/attachment.cgi?id=" + str(i)
+	real_url = url
+	connection.request( "GET", url )
+	response = connection.getresponse()
+
+	while response.status in [301, 302]:
+		real_url = response.getheader( "Location" )
+		connection.request( "GET", real_url )
+		response = connection.getresponse()
+
+	if response.status != 200:
+		print "%d: %s" % ( response.status, response.reason )
+		break
+
+	info = response.getheaders()
+	type = response.getheader( "Content-Type" )
 
 	if type_re.search( type ):
 		if patch_re.search( type ):
 			f = open( "%s/%d.patch" % (destdir,i), "w+" )
-			f.write( attachment.read() )
+			f.write( response.read() )
 			f.close()
 			f = open( "%s/%d.txt" % (destdir,i), "w+" )
-			f.write( "URL: " + url + "\n" )
-			f.write( str( info ) )
+			f.write( "Initial URL: http://%s%s\n" % ( base_url, url ) )
+			f.write( "Final URL: http://%s%s\n" % ( base_url, real_url ) )
+			for header in info:
+				line = "%s: %s\n" % (header)
+				f.write( line )
 			f.close()
 			print "Ok"
 		else:
-			print "No patch found"
+			print "Err, no patch found"
+			print info
 	else:
-		print "No patch found"
+		print "Err, no patch found"
+		print info
+
+connection.close()
